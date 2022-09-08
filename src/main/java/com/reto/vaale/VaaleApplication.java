d6c9ea617bfbb0b66796ed90409cfcc5abeeffec
Edward Daniel Vidal García <asistic10@cohan.org.co>
2022-09-08 11:30:37 -0500

    carga inicial proyecto

---
+++ src/main/java/com/reto/vaale/VaaleApplication.java
@@ -0,0 +1,13 @@
+package com.reto.vaale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reto/vaale/VaaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reto.vaale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
